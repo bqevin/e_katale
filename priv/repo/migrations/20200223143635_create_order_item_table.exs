2031bbc5eb6a236ef8f599a7756a2d5edd665e08
Maseline <mmusimire@gmail.com>
2020-02-25 19:00:05 +0300

    Add function to add to cart

--- priv/repo/migrations/20200223143635_create_order_item_table.exs
+++ priv/repo/migrations/20200223143635_create_order_item_table.exs
@@ -2,8 +2,7 @@ defmodule EKatale.Repo.Migrations.CreateOrderItemTable do
   use Ecto.Migration
 
   def change do
-    create table(:order_items, primary_key: false) do
-      add(:id, :uuid, primary_key: true)
+    create table(:order_items) do
       add(:order_id, references(:orders, type: :uuid, on_delete: :delete_all))
       add(:product_id, references(:products, on_delete: :delete_all))
       add(:unit_price, :integer)
