50813e12c143b888a11b174da1436c9fe329b1b1
Maseline <mmusimire@gmail.com>
2020-07-01 11:49:55 +0300

    Update auth required endpoints

--- lib/e_katale_web/router.ex
+++ lib/e_katale_web/router.ex
@@ -9,7 +9,7 @@ defmodule EKataleWeb.Router do
     plug EKataleWeb.Auth.Pipeline
   end
 
-  scope "/api", EKataleWeb do
+  scope "/", EKataleWeb do
     pipe_through :api
 
     # users endpoints
@@ -18,19 +18,26 @@ defmodule EKataleWeb.Router do
 
     # category endoints
     get("/categories", CategoryController, :index)
-    post("/categories", CategoryController, :create)
     get("/categories/:id", CategoryController, :show)
-    put("/categories/:id", CategoryController, :update)
     
     # products endpoints
     get("/products", ProductController, :index)
-    post("/products", ProductController, :create)
     get("/products/:id", ProductController, :show)
-    put("products/:id", ProductController, :update)
+
+    # require authentication for all endpoints below
+    # pipe_through [:auth]
   end
 
-  scope "/api", EKataleWeb do
+  scope "/admin", EKataleWeb do
     pipe_through [:api, :auth]
+
+    # categories endpoints
+    post("/categories", CategoryController, :create)
+    put("/categories/:id", CategoryController, :update)
+
+    # products endpoints
+    post("/products", ProductController, :create)
+    put("products/:id", ProductController, :update)
   end
 end
  
